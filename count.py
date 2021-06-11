f3df9d9c7c4736cb057f8755e523e1308ccae8a5
Leo brunel <55917959+Wizard-collab@users.noreply.github.com>
2021-06-11 16:53:34 +0200

    updated count.py

--- count.py
+++ count.py
@@ -7,9 +7,12 @@ for root, dirs, files in os.walk(os.path.abspath(""), topdown=False):
     for name in files:
         file_name = os.path.join(root, name)
         if not (('.git' in file_name) or ('pycache' in file_name) or ('pyc' in file_name)):
-            with open(file_name, 'r') as f:
-                data = f.read()
-                total_chars += len(data)
+            try:
+                with open(file_name, 'r') as f:
+                    data = f.read()
+                    total_chars += len(data)
+            except:
+                print(f"error : {file_name}")
             total_files += 1
 
 print(f"Your app contain {total_chars} characters")
