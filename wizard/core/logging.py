db8b6636680e936dfe6d2faa15a80711cd8801fd
Leo brunel <55917959+Wizard-collab@users.noreply.github.com>
2021-06-11 17:44:53 +0200

    logging file handler

--- wizard/core/logging.py
+++ wizard/core/logging.py
@@ -12,17 +12,14 @@ def get_logger(name=None):
     # create logger
     # Use this module to keep logging format
     # integrity across wizard application
-    
     logging_level = logging.INFO
-
-    file = user_vars._user_logging_file_
-
     logging.basicConfig(level=logging_level,
         format="%(asctime)s [%(name)-23.23s] [%(levelname)-5.5s] %(message)s")
-    
     if name:
         logger = logging.getLogger(name)
     else:
         logger = logging.getLogger()
-
+    file_handler = logging.FileHandler(user_vars._user_logging_file_)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s [%(name)-23.23s] [%(levelname)-5.5s] %(message)s'))
+    logger.addHandler(file_handler)
     return logger
