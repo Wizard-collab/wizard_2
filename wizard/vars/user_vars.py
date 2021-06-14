e2d3396bc1b79066d0bdc668f22897a8824a2f30
Leo brunel <55917959+Wizard-collab@users.noreply.github.com>
2021-06-14 17:15:30 +0200

    replaced user document path command with os.path.expanduser('~')

--- wizard/vars/user_vars.py
+++ wizard/vars/user_vars.py
@@ -6,7 +6,7 @@
 import os
 
 # User constants
-_user_path_ = '{}/Documents/wizard_2/'.format(os.getenv("USERPROFILE"))
+_user_path_ = os.path.expanduser('~/Documents/wizard_2/')
 _user_prefs_file_ = os.path.join(_user_path_, 'preferences.yaml')
 _user_logging_file_ = os.path.join(_user_path_, 'main.log')
 
