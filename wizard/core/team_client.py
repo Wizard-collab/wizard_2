2113702ce884e02554ffcae5f76ec4907e6487ef
Leo brunel <55917959+Wizard-collab@users.noreply.github.com>
2021-08-25 17:46:45 +0200

    25-08-2021-6

--- wizard/core/team_client.py
+++ wizard/core/team_client.py
@@ -82,14 +82,15 @@ class team_client(QThread):
         self.team_connection_status_signal.emit(False)
 
     def analyse_signal(self, data):
-        if data['type'] == 'refresh_team':
-            self.refresh_signal.emit(1)
-        elif data['type'] == 'notification':
-            self.notification_signal.emit(signal_dic['event_id'])
-        elif data['type'] == 'new_user':
-            self.new_user_signal.emit(data['user_name'])
-        elif data['type'] == 'remove_user':
-            self.remove_user_signal.emit(data['user_name'])
+        if data['project'] == environment.get_project_name():
+            if data['type'] == 'refresh_team':
+                self.refresh_signal.emit(1)
+            elif data['type'] == 'notification':
+                self.notification_signal.emit(signal_dic['event_id'])
+            elif data['type'] == 'new_user':
+                self.new_user_signal.emit(data['user_name'])
+            elif data['type'] == 'remove_user':
+                self.remove_user_signal.emit(data['user_name'])
 
 def try_connection(DNS):
     signal_dic = dict()
