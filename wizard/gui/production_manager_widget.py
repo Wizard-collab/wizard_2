dbb95ff9da692b45ff47baf3aa2a647623e5191c
Leo brunel <55917959+Wizard-collab@users.noreply.github.com>
2021-10-28 22:35:53 +0200

    10-28-2021-4

--- wizard/gui/production_manager_widget.py
+++ wizard/gui/production_manager_widget.py
@@ -4,6 +4,8 @@
 
 # Python modules
 from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtCore import pyqtSignal
+import time
 
 # Wizard gui modules
 from wizard.gui import gui_utils
@@ -11,6 +13,7 @@ from wizard.gui import gui_utils
 # Wizard modules
 from wizard.core import site
 from wizard.core import project
+from wizard.core import image
 from wizard.vars import ressources
 from wizard.vars import assets_vars
 
@@ -22,14 +25,6 @@ class production_manager_widget(QtWidgets.QWidget):
         self.asset_ids = dict()
         self.stage_ids = dict()
         self.variant_ids = dict()
-        self.refresh_users_dic()
-        self.refresh()
-
-    def refresh_users_dic(self):
-        users_ids = project.get_users_ids_list()
-        for user_id in users_ids:
-            if user_id not in self.users_ids.keys():
-                self.users_ids[user_id] = site.get_user_data(user_id, 'user_name')
 
     def build_ui(self):
 
@@ -59,33 +54,42 @@ class production_manager_widget(QtWidgets.QWidget):
         self.list_view.setHeaderLabels(['Asset', 'Modeling', 'Rigging', 'Grooming', 'Texturing', 'Shading'])
         self.main_layout.addWidget(self.list_view)
 
+        self.refresh_label = QtWidgets.QLabel()
+        self.refresh_label.setObjectName('tree_datas_label')
+        self.main_layout.addWidget(self.refresh_label)
+
+    def update_refresh_time(self, start_time):
+        refresh_time = str(round((time.time()-start_time), 3))
+        self.refresh_label.setText(f"refresh : {refresh_time}s")
+
     def refresh(self):
+        start_time = time.time()
+
         asset_rows = project.get_all_assets() 
         stage_rows = project.get_all_stages()
         variant_rows = project.get_all_variants()
 
         for asset_row in asset_rows:
-            self.asset_ids[asset_row['id']] = dict()
-            self.asset_ids[asset_row['id']]['row'] = asset_row
+            if asset_row['id'] not in self.asset_ids.keys():
+                self.asset_ids[asset_row['id']] = dict()
+                self.asset_ids[asset_row['id']]['row'] = asset_row
+                item = custom_asset_listWidgetItem(self.asset_ids[asset_row['id']]['row'], self.list_view.invisibleRootItem())
+                self.asset_ids[asset_row['id']]['item'] = item
 
         for stage_row in stage_rows:
-            self.stage_ids[stage_row['id']] = dict()
-            self.stage_ids[stage_row['id']]['row'] = stage_row
+            if stage_row['id'] not in self.stage_ids.keys():
+                self.stage_ids[stage_row['id']] = dict()
+                self.stage_ids[stage_row['id']]['row'] = stage_row
+                self.asset_ids[self.stage_ids[stage_row['id']]['row']['asset_id']]['item'].add_stage(self.stage_ids[stage_row['id']]['row'])
+                self.stage_ids[stage_row['id']]['asset_item'] = self.asset_ids[self.stage_ids[stage_row['id']]['row']['asset_id']]['item']
 
         for variant_row in variant_rows:
-            self.variant_ids[variant_row['id']] = dict()
-            self.variant_ids[variant_row['id']]['row'] = variant_row
+            if variant_row['id'] not in self.variant_ids.keys():
+                self.variant_ids[variant_row['id']] = dict()
+                self.variant_ids[variant_row['id']]['row'] = variant_row
+                self.stage_ids[self.variant_ids[variant_row['id']]['row']['stage_id']]['asset_item'].add_variant(self.variant_ids[variant_row['id']]['row'])
 
-        for asset_id in self.asset_ids.keys():
-            item = custom_asset_listWidgetItem(self.asset_ids[asset_id]['row'], self.list_view.invisibleRootItem())
-            self.asset_ids[asset_id]['item'] = item
-
-        for stage_id in self.stage_ids.keys():
-            self.asset_ids[self.stage_ids[stage_id]['row']['asset_id']]['item'].add_stage(self.stage_ids[stage_id]['row'])
-            self.stage_ids[stage_id]['asset_item'] = self.asset_ids[self.stage_ids[stage_id]['row']['asset_id']]['item']
-
-        for variant_id in self.variant_ids.keys():
-            self.stage_ids[self.variant_ids[variant_id]['row']['stage_id']]['asset_item'].add_variant(self.variant_ids[variant_id]['row'], users_ids=self.users_ids)
+        self.update_refresh_time(start_time)
 
 class custom_asset_listWidgetItem(QtWidgets.QTreeWidgetItem):
     def __init__(self, asset_row, parent=None):
@@ -93,24 +97,33 @@ class custom_asset_listWidgetItem(QtWidgets.QTreeWidgetItem):
         self.setText(0, asset_row['name'])
         self.stage_ids = dict()
         self.variant_ids = dict()
-    
+
     def add_stage(self, stage_row):
         self.stage_ids[stage_row['id']] = dict()
         self.stage_ids[stage_row['id']]['row'] = stage_row
-        widget = stage_widget(stage_row)
+        widget = stage_widget(stage_row, self.treeWidget())
         self.stage_ids[stage_row['id']]['widget'] = widget
         index = assets_vars._assets_stages_list_.index(stage_row['name'])+1
+        self.stage_ids[stage_row['id']]['index'] = index
         self.treeWidget().setItemWidget(self, index, widget)
 
-    def add_variant(self, variant_row, users_ids):
-        widget = variant_widget(variant_row, users_ids)
+    def add_variant(self, variant_row):
+        widget = variant_widget(variant_row, self.treeWidget())
+        widget.update_size.connect(self.update_size)
         stage_widget = self.stage_ids[variant_row['stage_id']]['widget']
         stage_widget.add_variant(widget)
         self.variant_ids[variant_row['id']] = dict()
         self.variant_ids[variant_row['id']]['row'] = variant_row
         self.variant_ids[variant_row['id']]['widget'] = widget
 
+    def update_size(self):
+        for stage_id in self.stage_ids:
+            widget = self.stage_ids[stage_id]['widget']
+            index = self.stage_ids[stage_id]['index']
+            self.setSizeHint(index, widget.sizeHint())
+
 class stage_widget(QtWidgets.QWidget):
+
     def __init__(self, stage_row, parent=None):
         super(stage_widget, self).__init__(parent)
         self.stage_row = stage_row
@@ -126,45 +139,53 @@ class stage_widget(QtWidgets.QWidget):
 
     def add_variant(self, variant_widget):
         self.main_layout.insertWidget(self.main_layout.count() - 1, variant_widget)
-        self.updateGeometry()
 
 class variant_widget(QtWidgets.QFrame):
-    def __init__(self, variant_row, users_ids, parent=None):
+
+    update_size = pyqtSignal(int)
+    
+    def __init__(self, variant_row, parent=None):
         super(variant_widget, self).__init__(parent)
         self.variant_row = variant_row
-        self.users_ids = users_ids
         self.build_ui()
+
+    def showEvent(self, event):
+        self.update_size.emit(1)
         self.fill_ui()
 
     def build_ui(self):
         self.setObjectName('production_manager_variant_frame')
         self.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
         self.main_layout = QtWidgets.QVBoxLayout()
+        self.main_layout.setContentsMargins(8,8,8,8)
+        self.main_layout.setSpacing(4)
         self.setLayout(self.main_layout)
 
         self.main_layout.addWidget(QtWidgets.QLabel(self.variant_row['name']))
 
-        self.combos_widget = QtWidgets.QWidget()
-        self.combos_widget.setObjectName('transparent_widget')
-        self.combos_layout = QtWidgets.QHBoxLayout()
-        self.combos_layout.setContentsMargins(0,0,0,0)
-        self.combos_layout.setSpacing(6)
-        self.combos_widget.setLayout(self.combos_layout)
-        self.main_layout.addWidget(self.combos_widget)
+        self.datas_widget = QtWidgets.QFrame()
+        #self.datas_widget.setObjectName('dark_widget')
+        self.datas_widget.setStyleSheet('border-radius:19px;')
+        print(self.datas_widget.size())
+        self.datas_layout = QtWidgets.QHBoxLayout()
+        self.datas_layout.setContentsMargins(5,5,5,5)
+        self.datas_layout.setSpacing(8)
+        self.datas_widget.setLayout(self.datas_layout)
+        self.main_layout.addWidget(self.datas_widget)
 
-        self.state_comboBox = state_comboBox()
-        self.combos_layout.addWidget(self.state_comboBox)
+        self.user_image_label = QtWidgets.QLabel()
+        self.user_image_label.setFixedSize(QtCore.QSize(30,30))
+        self.datas_layout.addWidget(self.user_image_label)
 
-        self.assignment_comboBox = gui_utils.QComboBox()
-        self.combos_layout.addWidget(self.assignment_comboBox)
-        for user_id in self.users_ids.keys():
-            self.assignment_comboBox.addItem(self.users_ids[user_id])
+        self.state_label = QtWidgets.QLabel()
+        self.state_label.setObjectName('bold_label')
+        self.datas_layout.addWidget(self.state_label)
 
         self.progress_bar_widget = QtWidgets.QWidget()
         self.progress_bar_widget.setObjectName('transparent_widget')
         self.progress_bar_layout = QtWidgets.QHBoxLayout()
         self.progress_bar_layout.setContentsMargins(0,0,0,0)
-        self.progress_bar_layout.setSpacing(6)
+        self.progress_bar_layout.setSpacing(4)
         self.progress_bar_widget.setLayout(self.progress_bar_layout)
         self.main_layout.addWidget(self.progress_bar_widget)
 
@@ -176,8 +197,10 @@ class variant_widget(QtWidgets.QFrame):
         self.progress_bar_layout.addWidget(self.percent_label)
 
     def fill_ui(self):
-        self.state_comboBox.setCurrentText(self.variant_row['state'])
-        self.assignment_comboBox.setCurrentText(self.variant_row['assignment'])
+        self.state_label.setText(self.variant_row['state'])
+        user_image =  site.get_user_row_by_name(self.variant_row['assignment'], 'profile_picture')
+        pm = gui_utils.mask_image(image.convert_str_data_to_image_bytes(user_image), 'png', 30)
+        self.user_image_label.setPixmap(pm)
 
         if self.variant_row['estimated_time'] is not None:
             percent = (float(self.variant_row['work_time'])/float(self.variant_row['estimated_time']))*100
@@ -194,14 +217,3 @@ class variant_widget(QtWidgets.QFrame):
         else:
             self.time_progress_bar.setValue(0)
             self.percent_label.setText("0%")
-
-class state_comboBox(gui_utils.QComboBox):
-    def __init__(self, parent=None):
-        super(state_comboBox, self).__init__(parent)
-        self.setIconSize(QtCore.QSize(14,14))
-        self.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
-        self.setFixedWidth(90)
-        self.addItem(QtGui.QIcon(ressources._state_todo_), assets_vars._asset_state_todo_)
-        self.addItem(QtGui.QIcon(ressources._state_wip_), assets_vars._asset_state_wip_)
-        self.addItem(QtGui.QIcon(ressources._state_done_), assets_vars._asset_state_done_ )
-        self.addItem(QtGui.QIcon(ressources._state_error_), assets_vars._asset_state_error_)
