ce2d4784a94cb8acdac2900645f06dc0b1ce609d
Leo brunel <55917959+Wizard-collab@users.noreply.github.com>
2022-06-30 22:09:04 +0200

    6-30-2022-2

--- wizard/gui/chart_utils.py
+++ wizard/gui/chart_utils.py
@@ -38,35 +38,69 @@ class pie_chart(QtWidgets.QWidget):
 class curves_chart(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(curves_chart, self).__init__(parent)
+        self.lines = dict()
+        self.add_line()
+
+    def add_line(self):
+        line = [(0, 0), (25,25), (50,55), (65, 80), (100,85)]
+        color = 'orange'
+        thickness = 4
+        name = 'test'
+        self.lines[name] = dict()
+        self.lines[name]['data'] = line
+        self.lines[name]['color'] = color
+        self.lines[name]['thickness'] = thickness
+
+        line = [(0, 10), (25,20), (50,75), (65, 80), (100,95)]
+        color = 'gray'
+        thickness = 2
+        name = 'test_2'
+        self.lines[name] = dict()
+        self.lines[name]['data'] = line
+        self.lines[name]['color'] = color
+        self.lines[name]['thickness'] = thickness
 
     def paintEvent(self, event):
-        try:
-            painter = QtGui.QPainter(self)
-            painter.setRenderHint(QtGui.QPainter.Antialiasing)
-            painter.setRenderHint(QtGui.QPainter.HighQualityAntialiasing)
+        painter = QtGui.QPainter(self)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        painter.setRenderHint(QtGui.QPainter.HighQualityAntialiasing)
+        m = 20
+        w = self.width()
+        h = self.height()
+        self.draw_grid(painter, m, w, h)
+        for line in self.lines.keys():
+            self.draw_line(self.lines[line], painter, m, w, h)
 
-            m = 20
-            w = self.width()
-            h = self.height()
+    def draw_line(self, line_dic, painter, m, w, h):
+        data_len = len(line_dic['data'])
+        if data_len >= 2:
+            old_point = None
+            for point in line_dic['data']:
+                if not old_point:
+                    old_point = point
+                else:
+                    pen = QtGui.QPen()
+                    pen.setBrush(QtGui.QColor(line_dic['color']))
+                    pen.setWidth(line_dic['thickness'])
+                    pen.setCapStyle(QtCore.Qt.RoundCap)
+                    pen.setJoinStyle(QtCore.Qt.RoundJoin)
+                    painter.setPen(pen)
+                    painter.drawLine(((w-m*2)*old_point[0])/100+m,
+                                        h-(((h-m*2)*old_point[1])/100+m),
+                                        ((w-m*2)*point[0])/100+m, 
+                                        h-(((h-m*2)*point[1])/100+m))
+                    old_point = point
 
-            # Draw grid
-            painter.setPen(QtGui.QPen(QtGui.QColor(255,255,255,10), 1))
-            painter.drawLine(w/2, m, w/2, h-m)
-            painter.drawLine((w-m*2)/4+m, m, (w-m*2)/4+m, h-m)
-            painter.drawLine(((w-m*2)/4)*3+m, m, ((w-m*2)/4)*3+m, h-m)
-            painter.drawLine(w-m, m, w-m, h-m)
-
-            painter.drawLine(m, m, w-m, m)
-            painter.drawLine(m, h/2, w-m, h/2)
-            painter.drawLine(m, (h-m*2)/4+m, w-m, (h-m*2)/4+m)
-            painter.drawLine(m, ((h-m*2)/4)*3+m, w-m, ((h-m*2)/4)*3+m)
-
-            # Ordinate
-            painter.setPen(QtGui.QPen(QtGui.QColor('gray'), 1))
-            painter.drawLine(m, m, m, h-m)
-
-            # Abscissa
-            painter.drawLine(m, h-m, w-m, h-m)
-
-        except:
-            logger.error(str(traceback.format_exc()))
+    def draw_grid(self, painter, m, w, h):
+        painter.setPen(QtGui.QPen(QtGui.QColor(255,255,255,10), 1))
+        painter.drawLine(w/2, m, w/2, h-m)
+        painter.drawLine((w-m*2)/4+m, m, (w-m*2)/4+m, h-m)
+        painter.drawLine(((w-m*2)/4)*3+m, m, ((w-m*2)/4)*3+m, h-m)
+        painter.drawLine(w-m, m, w-m, h-m)
+        painter.drawLine(m, m, w-m, m)
+        painter.drawLine(m, h/2, w-m, h/2)
+        painter.drawLine(m, (h-m*2)/4+m, w-m, (h-m*2)/4+m)
+        painter.drawLine(m, ((h-m*2)/4)*3+m, w-m, ((h-m*2)/4)*3+m)
+        painter.setPen(QtGui.QPen(QtGui.QColor('gray'), 1))
+        painter.drawLine(m, m, m, h-m)
+        painter.drawLine(m, h-m, w-m, h-m)
