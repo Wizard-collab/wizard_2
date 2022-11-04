94284264836c5db5f4a98fad53c9d261448196ad
Leo brunel <55917959+Wizard-collab@users.noreply.github.com>
2022-11-04 17:32:53 +0100

    painter import sbsar

--- softwares/substance_painter_wizard/wizard_plugin.py
+++ softwares/substance_painter_wizard/wizard_plugin.py
@@ -17,7 +17,7 @@ import wizard_communicate
 from substance_painter_wizard import export_ui
 from substance_painter_wizard import substance_painter_export
 from substance_painter_wizard import wizard_project
-from substance_painter_wizard import wizard_references
+from substance_painter_wizard import wizard_reference
 
 def save():
     if not substance_painter.project.is_open():
@@ -72,14 +72,21 @@ def save_or_save_increment():
         copy_save(None)
 
 def init_project():
-    mesh_file_path = wizard_references.get_mesh_file()
+    mesh_file_path = wizard_reference.get_mesh_file()
     if mesh_file_path:
         wizard_project.init_project(mesh_file_path)
 
 def update_mesh():
-    mesh_file_path = wizard_references.get_mesh_file()
+    mesh_file_path = wizard_reference.get_mesh_file()
     if mesh_file_path:
         wizard_project.update_mesh(mesh_file_path)
+
+def import_texturing(references=None):
+    if not references:
+        references = wizard_communicate.get_references(int(os.environ['wizard_work_env_id']))
+    if 'texturing' in references.keys():
+        for reference in references['texturing']:
+            wizard_reference.import_texturing(reference)
 
 class tool_bar(QtWidgets.QMenu):
     def __init__(self):
@@ -91,15 +98,20 @@ class tool_bar(QtWidgets.QMenu):
         export_icon = os.path.abspath("icons/export.png")
         magic_icon = os.path.abspath("icons/all.png")
         modeling_icon = os.path.abspath("icons/modeling.png")
+        texturing_icon = os.path.abspath("icons/texturing.png")
+        import_icon = os.path.abspath("icons/import.png")
         update_icon = os.path.abspath("icons/update.png")
         self.save_action = self.addAction(QtGui.QIcon(save_icon), 'Save')
         self.export_action = self.addAction(QtGui.QIcon(export_icon), 'Export')
         self.init_project_action = self.addAction(QtGui.QIcon(magic_icon), 'Init project')
+        self.import_menu = self.addMenu(QtGui.QIcon(import_icon), 'Import')
+        self.import_texturing_action = self.import_menu.addAction(QtGui.QIcon(texturing_icon), 'Import/Update texturing')
         self.update_menu = self.addMenu(QtGui.QIcon(update_icon), 'Update')
         self.update_modeling_action = self.update_menu.addAction(QtGui.QIcon(modeling_icon), 'Update modeling')
         self.save_action.triggered.connect(save)
         self.export_action.triggered.connect(self.show_export_ui)
         self.init_project_action.triggered.connect(init_project)
+        self.import_texturing_action.triggered.connect(import_texturing)
         self.update_modeling_action.triggered.connect(update_mesh)
 
     def show_export_ui(self):
