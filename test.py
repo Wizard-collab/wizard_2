269f756b2682f86a017f2a80c13900110de24671
Leo brunel <55917959+Wizard-collab@users.noreply.github.com>
2023-02-22 22:39:15 +0100

    22-02-2022

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-import random
-import statistics
-
-l = list(range(0, 1000))
-percent_step = 100/len(l)
-weights_list = []
-for a in l:
-	index = l.index(a)+1
-	weight = 100-(percent_step*index)+percent_step/2
-	weights_list.append(weight)
-print(random.choices(l, weights=weights_list)[0])
+from wizard.core import game
